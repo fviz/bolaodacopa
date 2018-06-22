e77313fd3c1ad675dacd98b7fbd9a1402e342ca6
Francisco Vizentin <franciscovizentin@gmail.com>
2018-06-22 04:42:56 +0200

    Layout changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,20 +7,26 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class UserController extends Controller
-{
-    public function edit(Request $request) {
-	    $passtry = User::where('password', $request->pin)->first();
-	    $user = Auth::user();
-	    if ($user == $passtry) {
-		    return redirect('/profile/' . $user->id);
-	    } elseif ($passtry === null) {
-		    $user->name = $request->name;
-		    $user->password = $request->pin;
-		    $user->save();
-		    return redirect('/profile/' . $user->id);
-	    } else {
-		    return view('error', ['error' => "PIN não disponível."]);
-	    }
-    }
+class UserController extends Controller {
+
+	public function edit(Request $request) {
+		$passtry = User::where('password', $request->pin)->first();
+		$user = Auth::user();
+
+		if ($passtry === null)
+		{
+			$user->name = $request->name;
+			$user->password = $request->pin;
+			$user->save();
+
+			return redirect('/profile/' . $user->id);
+		} else
+		{
+			if ($user->id == $passtry->id)
+			{
+				return redirect('/profile/' . $user->id);
+			}
+			return view('error', ['error' => "PIN não disponível."]);
+		}
+	}
 }
